6f519379c94c000bf4b949d4468482b28f3e77ae
Chmper <kamil-wyszynski@wp.pl>
2019-03-10 22:09:08 +0100

    Creating Tag added - using Angular with Spring REST Api

--- src/main/java/routine/demo/repository/ColorRepository.java
+++
@@ -1,9 +0,0 @@
-package routine.demo.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import routine.demo.model.Color;
-
-@Repository
-public interface ColorRepository extends CrudRepository<Color, Integer> {
-}
